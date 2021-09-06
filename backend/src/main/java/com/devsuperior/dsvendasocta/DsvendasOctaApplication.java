025384a54c6a2d8ae1775e5b80efc721d7778488
Octacílio <octa.oca44@gmail.com>
2021-09-06 11:36:30 -0300

    Projetos Criados

---
+++ backend/src/main/java/com/devsuperior/dsvendasocta/DsvendasOctaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendasocta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasOctaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasOctaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsvendasocta/DsvendasOctaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendasocta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasOctaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
