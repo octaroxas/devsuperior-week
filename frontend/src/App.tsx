a3ddce6b40e089d9848c0d028eb8782380dd3d15
Octacílio <octa.oca44@gmail.com>
2021-09-06 15:42:40 -0300

    Componente BarChart (grafico de barras)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import NavBar from 'components/NavBar';
 import Footer from 'components/Footer';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
 
 function App() {
    return (
       <>
          <NavBar />
          <div className="container">
-            <h1 className="text-primary">Olá</h1>
+            <h1 className="text-primary py-3">Dashboard</h1>
+            <div className="row px-3">
+            <div className="col-sm-6">
+               <h5 className="text-center text-secondary">Taxa de Conversão em Vendas (%)</h5>
+                  <BarChart />
+               </div>
+               <div className="col-sm-6">
+               <h5>Taxa de Conversão em vendas</h5>
+                  <BarChart />
+               </div>
+            </div>
+
+            <div>
+               <h1 className="py-3 text-primary">Vendas Realizadas</h1>
+            </div>
             <DataTable />
          </div>
          <Footer />
