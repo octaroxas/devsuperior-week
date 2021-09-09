8a1b9cabcd1890be2daa12acb4dd841ca9e95d59
Octacílio <octa.oca44@gmail.com>
2021-09-09 20:51:59 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendasocta/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendasocta/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendasocta.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendasocta.dto.SaleDTO;
+import com.devsuperior.dsvendasocta.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendasocta.dto.SaleSumDTO;
 import com.devsuperior.dsvendasocta.entities.Sale;
 import com.devsuperior.dsvendasocta.repositories.SaleRepository;
 import com.devsuperior.dsvendasocta.repositories.SellerRepository;
@@ -29,6 +33,16 @@ public class SaleService {
 		/*
 		 * result é convertido para SellerDTO e retornado
 		 * */
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 
 }
