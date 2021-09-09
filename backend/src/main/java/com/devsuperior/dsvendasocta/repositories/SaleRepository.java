2bdeefa7dddf79b2b276165dbf448618c98102e4
Octacílio <octa.oca44@gmail.com>
2021-09-09 20:04:42 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendasocta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsvendasocta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsvendasocta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/devsuperior/dsvendasocta/service/SaleService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsvendasocta.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendasocta.dto.SaleDTO;
+import com.devsuperior.dsvendasocta.entities.Sale;
+import com.devsuperior.dsvendasocta.repositories.SaleRepository;
+import com.devsuperior.dsvendasocta.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository; // Responsável por acessar os dados
+										 // A classe de serviço chama o repositorio para acessar os dados
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		
+		sellerRepository.findAll(); //traz todos os vendedores para a memória (pois são poucos)
+		Page<Sale> result =  repository.findAll(pageable); //Armazena as entidades retornadas em result
+		return result.map(x -> new SaleDTO(x));
+		/*
+		 * result é convertido para SellerDTO e retornado
+		 * */
+	}
+
+}
