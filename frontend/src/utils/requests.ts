b9f6571ae3bc9953d2cba37808dd3d1bc74215fa
Octacílio <octa.oca44@gmail.com>
2021-09-11 16:47:00 -0300

    .

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -2,4 +2,4 @@ export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8
 /*Caso a variável REACT_APP_BACKEND_URL não esteja definida no ambiente atual onde a aplicação está rodando
    * A url que vai ser utilizada será a de localhost
    * Dessa forma o projeto pode ser rodado tanto em ambiente local quanto em produção
-*/
+*/ 
